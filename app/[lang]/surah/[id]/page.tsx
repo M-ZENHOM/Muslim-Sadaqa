25fbfb66f1e703c18227bc7f6b489e391cfa6178
ZENHOM <you@example.com>
2023-09-10 02:08:11 +0300

    Surah page style for Mobile Version 👌

--- app/[lang]/surah/[id]/page.tsx
+++ app/[lang]/surah/[id]/page.tsx
@@ -34,18 +34,8 @@ export default async function page({ params, searchParams, params: { lang } }: {
 
     return (
         <Wrapper >
-            <Card className='flex flex-col items-start md:items-center md:flex-col p-8 md:absolute md:left-4 md:space-y-10 md:h-[80vh] bg-muted '>
-                <Image src={QuranImg} width={200} height={200} priority alt='Quran img' className='hidden md:block' />
-                <div className='flex flex-col space-y-5 font-extrabold w-full'>
-                    <h2>{lang === "ar" ? Surah.data?.name : Surah.data?.englishName} </h2>
-                    {lang === "en" && <h4>{Surah.data?.englishNameTranslation}</h4>}
-                    <span>{Surah.data?.numberOfAyahs} {SurahPage.Ayahs}</span>
-                    <span>{lang === "ar" ? Surah.data?.revelationType === "Medinan" ? "مدنيه" : "مكيه" : Surah.data?.revelationType}</span>
-                    <Link className={cn(buttonVariants({ variant: "default" }), { 'pointer-events-none opacity-50': Surah.data.number === 114 })} href={{ pathname: `/surah/${Surah.data.number + 1}` }} >{SurahPage.surahBtn}</Link>
-                    <Link className={cn(buttonVariants({ variant: "default" }), { 'pointer-events-none opacity-50': Surah.data.number === 1 })} href={{ pathname: `/surah/${Surah.data.number - 1}` }} >{SurahPage.surahBtnTwo}</Link>
-                </div>
-            </Card>
-            <ScrollArea className="w-full h-[85vh] py-20">
+
+            <ScrollArea className="w-full h-[85vh] pb-20 pt-5">
                 <Icons.Bismallah className='text-center w-full' />
                 {Surah.data.ayahs.map((sur: SurahType) => (
                     <div key={sur.number} className='flex justify-between items-center w-full p-2 group'>
@@ -56,7 +46,18 @@ export default async function page({ params, searchParams, params: { lang } }: {
                     </div>
                 ))}
             </ScrollArea>
-            <audio src={`https://cdn.islamic.network/quran/audio/128/ar.alafasy/${ayahNum}.mp3`} autoPlay={ayahNum === 1 ? false : true} controls className=' w-full h-full min-h-[45px]' />
+            <audio src={`https://cdn.islamic.network/quran/audio/128/ar.alafasy/${ayahNum}.mp3`} autoPlay={ayahNum === 1 ? false : true} controls className=' w-full h-full min-h-[45px] my-10' />
+            <Card className='flex flex-col items-start md:items-center md:flex-col p-8 md:absolute md:left-4 md:top-20 md:space-y-10 md:h-[80vh] bg-muted   '>
+                <Image src={QuranImg} width={200} height={200} priority alt='Quran img' className='hidden md:block' />
+                <div className='flex flex-col space-y-5 font-extrabold w-full'>
+                    <h2>{lang === "ar" ? Surah.data?.name : Surah.data?.englishName} </h2>
+                    {lang === "en" && <h4>{Surah.data?.englishNameTranslation}</h4>}
+                    <span>{Surah.data?.numberOfAyahs} {SurahPage.Ayahs}</span>
+                    <span>{lang === "ar" ? Surah.data?.revelationType === "Medinan" ? "مدنيه" : "مكيه" : Surah.data?.revelationType}</span>
+                    <Link className={cn(buttonVariants({ variant: "default" }), { 'pointer-events-none opacity-50': Surah.data.number === 114 })} href={{ pathname: `/surah/${Surah.data.number + 1}` }} >{SurahPage.surahBtn}</Link>
+                    <Link className={cn(buttonVariants({ variant: "default" }), { 'pointer-events-none opacity-50': Surah.data.number === 1 })} href={{ pathname: `/surah/${Surah.data.number - 1}` }} >{SurahPage.surahBtnTwo}</Link>
+                </div>
+            </Card>
         </Wrapper>
     )
 }
