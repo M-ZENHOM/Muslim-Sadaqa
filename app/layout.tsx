cc86804551c3f95bdc9d609bf16705b6df71b51e
ZENHOM <you@example.com>
2023-09-17 01:57:09 +0300

    Layout merge confilct :(

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { Locale } from '@/i18n-config'
-import '../globals.css'
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Noto_Kufi_Arabic as SansArabic } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
 import SiteHeader from '@/components/layouts/SiteHeader'
 import SiteFooter from '@/components/layouts/SiteFooter'
 import { siteConfig } from '@/config/site'
+import Providers from './Providers'
 
 export const inter = Inter({ subsets: ['latin'] })
 export const arabicSans = SansArabic({ subsets: ['arabic'], weight: ['400'] })
@@ -14,14 +14,13 @@ export const arabicSans = SansArabic({ subsets: ['arabic'], weight: ['400'] })
 export const metadata: Metadata = {
   title: `${siteConfig.title}`,
   description: siteConfig.description,
-  manifest: 'https://quran-with-you.vercel.app/manifest.json'
 }
 
 export default function RootLayout({ children, params }: { children: React.ReactNode, params: { lang: Locale } }) {
   return (
-    <html dir={params.lang === "ar" ? "rtl" : "ltr"} lang={params.lang}>
+    <html dir="rtl" lang="ar">
       <body className={arabicSans.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <Providers>
           <main className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
             <aside className="fixed top-0 z-30 h-screen w-full shrink-0 md:sticky md:block">
               <SiteHeader lang={params.lang} />
@@ -29,7 +28,7 @@ export default function RootLayout({ children, params }: { children: React.React
             {children}
           </main>
           <SiteFooter />
-        </ThemeProvider >
+        </Providers>
       </body>
     </html>
   )
