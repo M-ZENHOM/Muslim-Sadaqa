8a8b7e07b8fb79b6069b4e5b067e36c509393c8d
ZENHOM <you@example.com>
2023-09-17 02:12:21 +0300

    Aside Width conflict for mobile version🤦‍♂️

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children, params }: { children: React.React
       <body className={arabicSans.className}>
         <Providers>
           <main className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-            <aside className="fixed top-0 z-30 h-screen w-full shrink-0 md:sticky md:block">
+            <aside className="fixed top-0 z-30  h-screen  shrink-0 md:sticky md:block">
               <SiteHeader lang={params.lang} />
             </aside>
             {children}
