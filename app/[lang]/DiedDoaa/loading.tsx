867279df8520ff7b9c612091b196e8d9c4521acc
ZENHOM <you@example.com>
2023-09-21 12:49:43 +0300

    DiedDoaa Loading page 🤯

--- app/[lang]/DiedDoaa/loading.tsx
+++ app/[lang]/DiedDoaa/loading.tsx
@@ -5,10 +5,10 @@ import React from 'react'
 export default function loading() {
     return (
         <Wrapper className='py-20'>
-            <Skeleton className='w-[200px] h-4' />
-            <div className='flex items-center'>
-                <Skeleton className='w-full' />
-                <Skeleton className='w-[150px] rounded-lg h-10' />
+            <Skeleton className='w-[400px] h-4' />
+            <div className='flex items-center mt-8'>
+                <Skeleton className='w-full h-4' />
+                <Skeleton className='w-[180px] rounded-lg h-10 mx-2' />
             </div>
         </Wrapper>
     )
