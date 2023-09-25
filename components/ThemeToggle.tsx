a379873477c6f6d759c740fe0eb65201a08c20b6
ZENHOM <you@example.com>
2023-09-25 22:59:57 +0300

    ThemeToggle Clicks 🚨

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -9,9 +9,15 @@ export function ThemeToggle() {
     const { theme, setTheme } = useTheme()
     return (
         <div className="flex items-center justify-center w-full">
-            <Button className={cn("w-full", theme === "light" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="light"><Icons.Sun className="w-4 h-4" onClick={() => setTheme("light")} /></Button>
-            <Button className={cn("w-full mx-1", theme === "dark" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="dark"><Icons.Moon className="w-4 h-4" onClick={() => setTheme("dark")} /></Button>
-            <Button className={cn("w-full", theme === "system" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="system"><Icons.System className="w-4 h-4" onClick={() => setTheme("system")} /></Button>
+            <Button className={cn("w-full", theme === "light" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="light" onClick={() => setTheme("light")}>
+                <Icons.Sun className="w-4 h-4" />
+            </Button>
+            <Button className={cn("w-full mx-1", theme === "dark" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="dark" onClick={() => setTheme("dark")}>
+                <Icons.Moon className="w-4 h-4" />
+            </Button>
+            <Button className={cn("w-full", theme === "system" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="system" onClick={() => setTheme("system")}>
+                <Icons.System className="w-4 h-4" />
+            </Button>
             <span className="sr-only">Toggle theme</span>
         </div>
     )
