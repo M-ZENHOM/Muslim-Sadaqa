fa9b95f51c5cf4683de88cf5d13ca82762f6b0eb
ZENHOM <you@example.com>
2023-09-26 15:30:19 +0300

    Production Testing

--- lib/getSurah.ts
+++ lib/getSurah.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getSurah = async (id: number) => {
     try {
-        const res = await axios.get(`${process.env.API_URL}/surah/${id}/ar.alafasy`)
+        const res = await axios.get(`https://api.quran.com/api/v4/chapters/${id}?language=ar`)
         return res.data
     } catch (error) {
         console.log(error);
