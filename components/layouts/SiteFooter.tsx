5da497673d03c956c8b9cb8f989ca6ed50d7850f
ZENHOM <you@example.com>
2023-09-26 18:02:03 +0300

    Lemme out!

--- components/layouts/SiteFooter.tsx
+++ components/layouts/SiteFooter.tsx
@@ -4,12 +4,12 @@ import { Separator } from '../ui/separator'
 
 function SiteFooter() {
     return (
-        <>
+        <footer>
             <Separator />
-            <footer className='flex flex-col items-center justify-center text-center text-xs md:text-sm bg-gradient-to-tl from-primary/25 to-50% h-20'>
+            <div className='flex flex-col items-center justify-center text-center text-xs md:text-sm bg-gradient-to-tl from-primary/25 to-50% h-20'>
                 <p>Built by <Link className='bg-clip-text text-transparent bg-gradient-to-r from-primary to-black dark:to-white' target='_blank' href="https://mahmoud-zenhom.tk/"> Mahmoud Zenhom</Link></p>
-            </footer>
-        </>
+            </div>
+        </footer>
     )
 }
 
