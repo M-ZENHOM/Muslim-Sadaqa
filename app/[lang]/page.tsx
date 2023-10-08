586b0d515e1c2b7f5880da249e41ced1755c27a7
ZENHOM <zenhom.dev@gmail.com>
2023-10-08 14:56:49 +0300

    QuranFahras Static

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -9,12 +9,13 @@ import { Locale } from '@/i18n-config'
 import { getDictionary } from './dictionaries'
 import { getMusliumData } from '@/lib/getMusliumData'
 import { type QuranData } from '@/types'
+import { QuranFahras } from '@/config/static/QuranFahras'
 
 
 export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
   const ayah = await getRandomAyah(Math.floor(Math.random() * 6236))
   const { IndexPage } = await getDictionary(lang)
-  const QuranFahras = await getMusliumData("QuranFahras")
+  // const QuranFahras = await getMusliumData("QuranFahras")
   return (
     <Wrapper>
       <Icons.QuranKareem className='w-4/12 mx-auto py-28 md:py-20' />
@@ -26,7 +27,7 @@ export default async function Home({ params: { lang } }: { params: { lang: Local
         <FavBox IndexPage={IndexPage} />
       </div>
       <div className={cn('grid grid-cols-fluid gap-4 py-4 place-items-center')}>
-        {QuranFahras.map((q: QuranData) => (
+        {QuranFahras?.data.map((q) => (
           <SurhaBox key={q.number} number={q.number} name={q.name} numberOfAyahs={q.numberOfAyahs} englishName={q.englishName} lang={lang} />
         ))}
       </div>
