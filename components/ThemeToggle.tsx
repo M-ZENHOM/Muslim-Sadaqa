f77689c7c8241e72a3df5d05f9bfe74cc02f3940
ZENHOM <zenhom.dev@gmail.com>
2023-12-20 09:52:49 +0200

    fix: wrong color and hover with theme change

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -9,13 +9,13 @@ export function ThemeToggle() {
     const { theme, setTheme } = useTheme()
     return (
         <div className="flex items-center justify-center w-full">
-            <Button className={cn("w-full", theme === "light" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="light" onClick={() => setTheme("light")}>
+            <Button className={cn("w-full", { "bg-primary text-primary-foreground": theme === "light" })} variant="outline" aria-label="Theme" value="light" onClick={() => setTheme("light")}>
                 <Icons.Sun className="w-4 h-4" />
             </Button>
-            <Button className={cn("w-full mx-1", theme === "dark" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="dark" onClick={() => setTheme("dark")}>
+            <Button className={cn("w-full mx-1", { "bg-primary text-primary-foreground": theme === "dark" })} variant="outline" aria-label="Theme" value="dark" onClick={() => setTheme("dark")}>
                 <Icons.Moon className="w-4 h-4" />
             </Button>
-            <Button className={cn("w-full", theme === "system" ? "bg-primary" : "")} variant="outline" aria-label="Theme" value="system" onClick={() => setTheme("system")}>
+            <Button className={cn("w-full", { "bg-primary text-primary-foreground": theme === "system" })} variant="outline" aria-label="Theme" value="system" onClick={() => setTheme("system")}>
                 <Icons.System className="w-4 h-4" />
             </Button>
             <span className="sr-only">Toggle theme</span>
