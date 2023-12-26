da6a7279710599acce2a0512eff475373ddf9327
ZENHOM <zenhom.dev@gmail.com>
2023-12-26 22:28:30 +0200

    feat: new componenet

--- components/layouts/MainNav.tsx
+++ components/layouts/MainNav.tsx
@@ -1,22 +1,27 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { ThemeToggle } from '../ThemeToggle'
 import LangToggle from '../LangToggle'
 import { cn } from '@/lib/utils'
 import { Locale } from '@/i18n-config'
+import { Badge } from '../ui/badge'
+import { usePathname } from 'next/navigation'
 
 
 function MainNav({ NavMenu, lang }: { NavMenu: { title: string, href: string, IsNew?: boolean }[], lang: Locale }) {
+    const pathName = usePathname()
     return (
-        <nav className='w-fit h-screen p-6 space-y-5 hidden md:flex md:flex-col'>
+        <nav className='w-[300px] h-screen p-6 space-y-5 hidden md:flex md:flex-col'>
             <ul className='flex flex-col space-y-3 text-lg font-extrabold border border-primary/50 rounded-lg p-6 bg-muted/25 bg-gradient-to-tr from-primary/25 to-50%'>
                 {NavMenu.map((link, i) => (
-                    <Link aria-label={link.title} key={i} className={cn('p-2 rounded-lg  hover:bg-primary/50 transition-all duration-300', { "bg-green-500/50": link.IsNew })} href={`/${lang}${link.href}`}>{link.title}</Link>
+                    <Link aria-label={link.title} key={i} className={cn('p-2 rounded-lg flex items-center transition-all duration-300 ', { "bg-primary text-primary-foreground": pathName === `/${lang}${link.href}` })} href={`/${lang}${link.href}`}>
+                        {link.title} {link.IsNew && <Badge className={cn({ "mr-2": lang === "ar", "ml-2": lang === "en" })} variant="new">{lang === 'ar' ? 'جديد' : 'new'}</Badge>}
+                    </Link>
                 ))}
             </ul>
             <div className='flex flex-col gap-4 w-full border border-primary/50 rounded-lg p-6 bg-muted/25 bg-gradient-to-tr from-primary/25 to-50%'>
                 <ThemeToggle />
-
             </div>
             <div className='flex flex-col gap-4 w-full border border-primary/50 rounded-lg p-6 bg-muted/25 bg-gradient-to-tr from-primary/25 to-50%'>
                 <LangToggle />
--- components/layouts/MobileNav.tsx
+++ components/layouts/MobileNav.tsx
@@ -1,45 +1,46 @@
 "use client"
 import React from 'react'
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '../ui/sheet'
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 import { cn } from '@/lib/utils'
 import { Icons } from '../Icons'
 import { buttonVariants } from '../ui/button'
 import Link from 'next/link'
 import { siteConfig } from '@/config/site'
 import { ThemeToggle } from '../ThemeToggle'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import LangToggle from '../LangToggle'
 import { Locale } from '@/i18n-config'
+import { Badge } from '../ui/badge'
 
 
 function MobileNav({ NavMenu, lang }: { NavMenu: { title: string, href: string, IsNew?: boolean }[], lang: Locale }) {
     const router = useRouter()
     const [open, setOpen] = React.useState(false)
+    const pathName = usePathname()
     const handleNavigate = (href: string) => {
         router.push(href)
         setOpen(false)
     }
     return (
-        <nav className='fixed top-0 md:hidden w-full bg-muted p-3 flex flex-row-reverse items-center justify-between'>
+        <nav dir={lang === "ar" ? "rtl" : "ltr"} className='fixed top-0 md:hidden w-full bg-muted p-3 flex  items-center justify-between'>
             <Link href='/'>{siteConfig.title}</Link>
             <Sheet open={open} onOpenChange={setOpen}>
                 <SheetTrigger className={cn(buttonVariants({ variant: "outline" }), "mx-2")}>
                     <Icons.Bars aria-label='Bars' />
                 </SheetTrigger>
-                <SheetContent>
-                    <SheetHeader className='py-5 '>
-                        <ul className='flex flex-col space-y-3 text-lg font-extrabold'>
-                            {NavMenu.map((link, i) => (
-                                <li aria-label={link.title} key={i}>
-                                    <button aria-label={link.title} onClick={() => handleNavigate(`/${lang}${link.href}`)} key={i} className={cn('bg-gradient-to-tl from-primary/25 to-60% p-2 rounded-lg hover:bg-primary/50 transition-all duration-300', { "bg-green-500/50": link.IsNew })} >{link.title}</button>
-                                </li>
-                            ))}
-                        </ul>
-                        <div className='flex flex-col items-baseline bottom-5 absolute space-y-5'>
-                            <ThemeToggle />
-                            <LangToggle />
-                        </div>
-                    </SheetHeader>
+                <SheetContent className='flex flex-col items-start space-y-5 py-16'>
+                    <ul className='flex flex-col  items-start space-y-3 text-lg font-extrabold'>
+                        {NavMenu.map((link, i) => (
+                            <li aria-label={link.title} key={i}>
+                                <button aria-label={link.title} onClick={() => handleNavigate(`/${lang}${link.href}`)} key={i} className={cn('bg-gradient-to-tl from-primary/25 to-60% p-2  rounded-lg hover:bg-primary/50 transition-all duration-300', { "flex items-center": link.IsNew, "bg-primary text-primary-foreground": pathName === `/${lang}${link.href}` })} >
+                                    {link.title}
+                                    {link.IsNew && <Badge className={cn({ "mr-2": lang === "ar", "ml-2": lang === "en" })} variant="new">{lang === 'ar' ? 'جديد' : 'new'}</Badge>}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                    <ThemeToggle />
+                    <LangToggle />
                 </SheetContent>
             </Sheet>
         </nav>
