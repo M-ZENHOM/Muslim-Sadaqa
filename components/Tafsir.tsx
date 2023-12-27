e31dd9e48c8eef3236b4810e8a09df9e91370074
ZENHOM <zenhom.dev@gmail.com>
2023-12-27 03:21:26 +0200

    feat: new component and new hook

--- components/Tafsir.tsx
+++ components/Tafsir.tsx
@@ -5,6 +5,9 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import { Skeleton } from './ui/skeleton'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer'
+import { Button } from './ui/button'
 
 interface TafsirProps {
     ayahNum: number
@@ -13,37 +16,64 @@ interface TafsirProps {
 }
 
 const Tafsir: FC<TafsirProps> = ({ ayahNum, ayahText, lang }) => {
+    const isDesktop = useMediaQuery("(min-width: 768px)")
     const { data, isLoading } = useQuery(["ayah", ayahNum, lang], () => axios.get(`https://api.alquran.cloud/v1/ayah/${ayahNum}/${lang === "ar" ? "ar.muyassar" : "en.asad"}`).then((res) => res.data.data));
+
+    if (isDesktop) {
+        return (
+            <Dialog>
+                <DialogTrigger ><Icons.Tafsir className='hover:bg-primary hover:rounded-sm hover:text-primary-foreground p-1 w-6 h-6' /></DialogTrigger>
+                {isLoading ? (
+                    <DialogContent className='p-10'>
+                        <Skeleton className='w-full h-6' />
+                        <Skeleton className='w-full h-6' />
+                        <div className='flex items-center gap-2'>
+                            <Skeleton className='w-[200px] h-10 rounded-lg' />
+                            <Skeleton className='w-[200px] h-10 rounded-lg' />
+                        </div>
+                    </DialogContent>
+                ) : (
+                    <DialogContent className='p-10' dir='rtl'>
+                        <DialogHeader className='space-y-5 text-start'>
+                            <DialogTitle className='leading-8'>
+                                {ayahText}
+                            </DialogTitle>
+                            <DialogDescription>
+                                {data?.text}
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className='flex items-center gap-2'>
+                            <span className='bg-muted rounded-lg w-fit p-2 px-4'>{data?.edition.name}</span>
+                            <span className='bg-muted rounded-lg w-fit p-2 px-4'> {lang === "ar" ? data?.surah.name : data?.surah.englishNameTranslation}</span>
+                        </div>
+                    </DialogContent>
+                )}
+            </Dialog >
+        )
+    }
     return (
-        <Dialog>
-            <DialogTrigger ><Icons.Tafsir className='hover:bg-primary hover:rounded-sm hover:text-primary-foreground p-1 w-6 h-6' /></DialogTrigger>
-            {isLoading ? (
-                <DialogContent className='p-10'>
-                    <Skeleton className='w-full h-6' />
-                    <Skeleton className='w-full h-6' />
-                    <div className='flex items-center gap-2'>
-                        <Skeleton className='w-[200px] h-10 rounded-lg' />
-                        <Skeleton className='w-[200px] h-10 rounded-lg' />
-                    </div>
-                </DialogContent>
-            ) : (
-                <DialogContent className='p-10' dir='rtl'>
-                    <DialogHeader className='space-y-5 text-start'>
-                        <DialogTitle className='leading-8'>
-                            {ayahText}
-                        </DialogTitle>
-                        <DialogDescription>
-                            {data?.text}
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className='flex items-center gap-2'>
-                        <span className='bg-muted rounded-lg w-fit p-2 px-4'>{data?.edition.name}</span>
-                        <span className='bg-muted rounded-lg w-fit p-2 px-4'> {lang === "ar" ? data?.surah.name : data?.surah.englishNameTranslation}</span>
-                    </div>
-                </DialogContent>
-            )
-            }
-        </Dialog >
+        <Drawer >
+            <DrawerTrigger asChild>
+                <Icons.Tafsir className='hover:bg-primary hover:rounded-sm hover:text-primary-foreground p-1 w-6 h-6' />
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader className="text-left">
+                    <DrawerTitle className='py-2'>{ayahText}</DrawerTitle>
+                    <DrawerDescription>
+                        {data?.text}
+                    </DrawerDescription>
+                </DrawerHeader>
+                <div className='flex items-center gap-2 py-6'>
+                    <span className='bg-muted rounded-lg w-fit p-2 px-4'>{data?.edition.name}</span>
+                    <span className='bg-muted rounded-lg w-fit p-2 px-4'> {lang === "ar" ? data?.surah.name : data?.surah.englishNameTranslation}</span>
+                </div>
+                <DrawerFooter className="pt-2">
+                    <DrawerClose asChild>
+                        <Button variant="outline">{lang === "ar" ? "اغلاق" : "Close"}</Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
     )
 }
 
