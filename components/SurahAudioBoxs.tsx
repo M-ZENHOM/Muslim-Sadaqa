2b440579502dec8a1a269bd991183a67fb2b4bb2
ZENHOM <zenhom.dev@gmail.com>
2024-03-05 17:43:40 +0200

    fix: audio player auto play

--- components/SurahAudioBoxs.tsx
+++ components/SurahAudioBoxs.tsx
@@ -14,14 +14,14 @@ export default function SurahAudioBoxs({ ayahNum, SurahPage, surahNum }: { ayahN
                 autoPlay={false}
                 style={{ height: '160px', }}
                 className={cn("custom-audio-player rounded-xl  bg-muted/25 bg-gradient-to-tr from-primary/25 to-50%")}
-                src={setting?.reciter && surahNum !== 0 ? `https://cdn.islamic.network/quran/audio-surah/128/${setting?.reciter}/${surahNum}.mp3` : "#"}
+                src={`https://cdn.islamic.network/quran/audio-surah/128/${setting?.reciter}/${surahNum}.mp3`}
             />
             <AudioPlayer
                 style={{ width: '100%', display: `${ayahNum === 0 ? 'none' : 'block'}` }}
                 className={cn("absolute bottom-0 left-[50%] translate-x-[-50%] custom-audio-player")}
                 layout='horizontal'
                 autoPlay={false}
-                src={`${`https://cdn.islamic.network/quran/audio/128/ar.alafasy/${ayahNum}.mp3`}`}
+                src={`https://cdn.islamic.network/quran/audio/128/ar.alafasy/${ayahNum}.mp3`}
             />
         </div>
     )
