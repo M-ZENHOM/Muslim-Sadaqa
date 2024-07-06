289826329fcda65227e6a0b71a9e4f49dee36e2a
ZENHOM <xmahmoud.zenhom@gmail.com>
2024-07-06 23:52:33 +0300

    style(quran-page): small text for randomAyah in long ayahs

--- components/TodayAyah.tsx
+++ components/TodayAyah.tsx
@@ -20,13 +20,16 @@ export default function TodayAyah({
   randomAyah: string;
 }) {
   const [_, copy] = useCopyToClipboard();
+  const textLength = randomAyah.length;
   return (
     <Card className="w-full h-60  p-8 text-center space-y-3 bg-gradient-to-br from-primary/25 to-50% flex flex-col items-center justify-center">
       <CardTitle className="text-muted-foreground">{AyahBoxTitle}</CardTitle>
       {!randomAyah ? (
         <Skeleton className="w-full h-10" />
       ) : (
-        <h2 className="text-lg">{randomAyah}</h2>
+        <h2 className={`${textLength > 50 ? "text-md" : "text-xl"}`}>
+          {randomAyah}
+        </h2>
       )}
       <Button className="flex" onClick={() => copy(randomAyah)}>
         {lang === "ar" ? "نسخ الاية" : "Copy Ayah"}{" "}
