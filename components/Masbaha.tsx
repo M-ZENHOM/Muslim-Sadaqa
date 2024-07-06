e616134975f387ab71d7e93ceebcb01029e26d1a
ZENHOM <xmahmoud.zenhom@gmail.com>
2024-07-07 01:28:40 +0300

    feat(masbha-page): remember count for next time and add new hook

--- components/Masbaha.tsx
+++ components/Masbaha.tsx
@@ -1,18 +1,35 @@
-"use client"
-import Image from 'next/image'
-import React from 'react'
-import { cn } from '@/lib/utils'
+"use client";
+import Image from "next/image";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useMounted } from "@/hooks/useMounted";
 
 const Masbaha = ({ className }: { className?: string }) => {
-    const [count, setCount] = React.useState(0)
-    return (
-        <div className={cn('relative flex justify-center items-center w-[400px] h-[400px]', className)}>
-            <Image className='relative' src='/Masbha.png' width={400} height={400} priority alt='Masbha' />
-            <span className='absolute text-5xl md:text-6xl top-[20%] md:top-[16%] blur-[1px]'>{count}</span>
-            <button onClick={() => setCount((prev) => prev + 1)} className='absolute top-[50%] translate-y-[40%] md:translate-y-[50%] bg-transparent w-32 h-32 rounded-full' />
-            <button onClick={() => setCount(0)} className='absolute top-[50%] translate-y-[40%] right-[20%] bg-transparent w-10 h-10 rounded-full' />
-        </div>
-    )
-}
+  const [count, setCount] = useLocalStorage("masbahaCount", 0);
+  const mounted = useMounted();
 
-export default Masbaha
+  return (
+    <div
+      className={cn(
+        "relative flex justify-center items-center w-[400px] h-[400px]",
+        className
+      )}
+    >
+      <Image className="relative" src="/Masbha.png" width={400} height={400} priority alt="Masbha" />
+      <span className="absolute text-5xl md:text-6xl top-[20%] md:top-[16%] blur-[1px]">
+        {mounted ? count : 0}
+      </span>
+      <button
+        onClick={() => setCount((prev) => Number(prev) + 1)}
+        className="absolute top-[50%] translate-y-[40%] md:translate-y-[50%] bg-transparent w-32 h-32 rounded-full"
+      />
+      <button
+        onClick={() => setCount(0)}
+        className="absolute top-[50%] translate-y-[40%] right-[20%] bg-transparent w-10 h-10 rounded-full"
+      />
+    </div>
+  );
+};
+
+export default Masbaha;
--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,14 +1,28 @@
 import React from "react"
 
-export const useLocalStorage = (key: string, initailValue: string) => {
-    const [value, setValue] = React.useState(() => {
-        const storedValue = typeof window !== 'undefined' && window.localStorage.getItem(key)
-        return storedValue && typeof window !== 'undefined' ? JSON.parse(storedValue) : initailValue
-    })
+export const useLocalStorage = (key: string, initialValue: string | number) => {
+    const [storedValue, setStoredValue] = React.useState(initialValue);
 
     React.useEffect(() => {
-        typeof window !== 'undefined' && window.localStorage.setItem(key, JSON.stringify(value))
-    }, [value])
+        try {
+            const item = window.localStorage.getItem(key);
+            if (item) {
+                setStoredValue(JSON.parse(item));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }, [key]);
 
-    return [value, setValue]
+    const setValue = (value: string | number | ((val: string | number) => string | number)) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return [storedValue, setValue] as const;
 }
